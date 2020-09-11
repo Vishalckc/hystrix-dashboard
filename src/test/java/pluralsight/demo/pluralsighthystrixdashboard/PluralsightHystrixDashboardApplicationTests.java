5f737a48c550a549ffbbf1bf80546db511027a59
Vishalckc <vishalvermackc@gmail.com>
2020-09-11 11:31:40 +0100

    First Commit

---
+++ src/test/java/pluralsight/demo/pluralsighthystrixdashboard/PluralsightHystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package pluralsight.demo.pluralsighthystrixdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PluralsightHystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
